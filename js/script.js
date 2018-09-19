d7dcf7aa4573324a1aff37cc24beec0efccbb0c4
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-19 16:40:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $(".button").click(function(){
   $("body").css("background-image","url('https://images.pexels.com/photos/1092364/pexels-photo-1092364.jpeg')");
-  }
-}
+  });
+});
+
