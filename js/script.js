c03b77c137f3d370a92e434ad88837424a83adf8
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-19 16:07:36 -0500

    Delete script.js

--- js/script.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $('button').click(function(){
-  $('body').css('background','url(https://images.pexels.com/photos/1092364/pexels-photo-1092364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260)');
-  }
-}
