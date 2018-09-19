aa470e890d8fa78ea6671704c28db67534e03c17
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-19 16:44:42 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-  $("button").click(function(){
-  $("body").css("background-image","url('https://images.pexels.com/photos/1092364/pexels-photo-1092364.jpeg')");
-  });
+	$("button").click(function(){
+	$("body").css("background-image","url('https://images.pexels.com/photos/1092364/pexels-photo-1092364.jpeg')");
+	});
 });
+				
 
