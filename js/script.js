8b28df8fefb5e28df9cf086829dd85581dfe3cec
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-20 14:31:54 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
-    $("body").css({"background-color": "#6B0200", "color":"#fff"});
-    $("button").click(function(){
-        $("body").toggleClass("fall");
-        $("header, footer").toggleClass("text");
-        $("nav").toggleClass("navigation");
-        $("a").toggleClass("links");
-        $("article").toggleClass("content");
-        $("#update").toggleClass("hide");
+       $("button").click(function(){
+            $("body").toggleClass("fall");
+            $("header, footer").toggleClass("text");
+            $("nav").toggleClass("navigation");
+            $("a").toggleClass("links");
+            $("article").toggleClass("content");
+            $("#update").toggleClass("hide");
     });
 });			
 
