11c6f6353239f7397e9feeacabb1509b5b40ddad
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-20 13:42:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $("button").click(function(){
         $("body").toggleClass("fall");
         $("header").toggleClass("text");
+        $("nav").toggleClass("navigation");
+        $("article").toggleClass("content");
     });
 });			
 
