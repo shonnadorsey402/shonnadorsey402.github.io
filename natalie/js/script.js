8ed5868e97f7d2d5530918ecb494b20cc6dbee16
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:11:03 -0500

    Update script.js

--- natalie/js/script.js
+++ natalie/js/script.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
-  $('img').fadeIn(slow);
-  $('button').click(function(){
-    $('img').hide());
-    });
+  $("button").click(function(){
+    $("body").toggleClass("fall");
+    $("header", footer").toggleClass('text");
+    $("nav").toggleClass("navigations");
+    $("a").toggleClass("links");
+    $("artical").toggleClass("content");
+    $("#update").toggleClass("hide");
+  });
 });
   
   
