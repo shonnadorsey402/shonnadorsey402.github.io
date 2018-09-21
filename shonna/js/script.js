1c859a5f08f5f968a27abbcda87dfd6056b28957
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:20:17 -0500

    Update script.js

--- shonna/js/script.js
+++ shonna/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-       $("button").click(function(){
-            $("body").toggleClass("fall");
-            $("header, footer").toggleClass("text");
-            $("nav").toggleClass("navigation");
-            $("a").toggleClass("links");
-            $("article").toggleClass("content");
-            $("#update").toggleClass("hide");
+       $('button').click(function(){
+            $('body').toggleClass('fall');
+            $('header, footer').toggleClass('text');
+            $('nav').toggleClass('navigation');
+            $('a').toggleClass('links');
+            $('article').toggleClass('content');
+            $('#update').toggleClass('hide');
     });
 });			
