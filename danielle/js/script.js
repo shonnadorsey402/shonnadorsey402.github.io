041e5c502a64463249e73296a6eeedaa711294f8
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:12:14 -0500

    Update script.js

--- danielle/js/script.js
+++ danielle/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-$('button').click(function(){
-    $('body').toggleClass("fall");
-    $('header.footer').toggleClass("text");
-    $('nav').toggleClass("navigation");
-    $('a').toggleClass("links");
-    $('article').toggleClass("content");
-    $('#update').toggleClass("hide");
+$("button").click(function(){
+    $("body").toggleClass("fall");
+    $("header.footer").toggleClass("text");
+    $("nav").toggleClass("navigation");
+    $("a").toggleClass("links");
+    $("article").toggleClass("content");
+    $("#update").toggleClass("hide");
 });
 });
     
