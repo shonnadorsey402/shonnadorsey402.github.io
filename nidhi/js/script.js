267fd333bdf2bb0c3c6485534e7e3a598558b526
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:21:48 -0500

    Update script.js

--- nidhi/js/script.js
+++ nidhi/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
-$('img').fadeIn(slow);
 $('button').click(function(){
-  $('img').hide();
+  $('body').toggleClass('Spring');
+  $('header,footer').toggleClass("text");
+  $('nav').toggleClass('navigation');
+  $('a').toggleClass('links');
+  $('article').toggleClass('content);
+  $('#update').toggleClass('hide');
 }) ;
 });
