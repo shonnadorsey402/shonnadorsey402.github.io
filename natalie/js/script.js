30b556b603b49d44484e4763d2c03868b028df84
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:15:40 -0500

    Update script.js

--- natalie/js/script.js
+++ natalie/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   $("button").click(function(){
     $("body").toggleClass("fall");
-    $("header", footer").toggleClass('text");
+    $("header", footer").toggleClass("text");
     $("nav").toggleClass("navigations");
     $("a").toggleClass("links");
-    $("artical").toggleClass("content");
+    $("article").toggleClass("content");
     $("#update").toggleClass("hide");
   });
 });
