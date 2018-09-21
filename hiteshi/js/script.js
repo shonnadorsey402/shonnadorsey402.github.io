98530eb66456a0cbab618b0bcd1e1258f2dc08f5
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-21 15:48:48 -0500

    Update script.js

--- hiteshi/js/script.js
+++ hiteshi/js/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    $("button").click(function(){
-       $("body").toggleClass("fall");
-       $("header' footer").toggleClass("text");
-       $("nav").toggleclass("navigation");
-       $("a").toggleclass("links");
-       $("article").toggleClass("content");
-       $("#update").toggleclass("hide");
+    $('button').click(function(){
+       $('body').toggleClass('fall');
+       $('header' 'footer').toggleClass('text');
+       $('nav').toggleclass('navigation');
+       $('a').toggleclass('links');
+       $('article').toggleClass('content');
+       $('#update').toggleclass('hide');
    });
       });
