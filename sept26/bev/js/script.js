8d360a59dc84cde2d8eed561432411f660aa632b
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:36:07 -0500

    Update script.js

--- sept26/bev/js/script.js
+++ sept26/bev/js/script.js
@@ -3,6 +3,6 @@ $('#round-corners').click(function(){
   $('div').css({'border-radius':'10em'});
 });
 $('#text').click(function(){
-  $('#custom-paragraph').css({background-color':'blue'});
+  $('#custom-paragraph').css({'background-color':'blue'});
 });
 });
