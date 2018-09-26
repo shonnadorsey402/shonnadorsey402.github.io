5b7729c5047add7f71cbd61e934fa4af807d4fed
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:23:21 -0500

    Update script.js

--- sept26/peggy/js/script.js
+++ sept26/peggy/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('#round-corners').click(function(){
+    $('div').css({'borders-radius':'10em'});
+  });
+  
+  $('text').click(function() {
+    $('p').css({'background-color':'red'});
+  });
 });
