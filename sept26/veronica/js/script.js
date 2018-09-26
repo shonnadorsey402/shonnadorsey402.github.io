b4dffab557f12f1610144a8428250d8334877cc9
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:22:22 -0500

    Update script.js

--- sept26/veronica/js/script.js
+++ sept26/veronica/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-  @$('#round-corners').click(function(){
+  $('#round-corners').click(function(){
     $('div').css({'border-radius' : '10em' });
+  });
+
+  $('#text').click(function(){
+    $('p').css({'background-color' : 'red'});
   });
