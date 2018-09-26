4794aa62a64d97efc7d0d523433676a939ca535b
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:22:15 -0500

    Update script.js

--- sept26/cassie/js/script.js
+++ sept26/cassie/js/script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+$('#round-corners').click(function(){
+ $('div').css({'border-radius':'10em'});                         
 });
+  
+  $('#text').click(function(){
+    $('p').css({'background-color': 'aqua'});
+               });
