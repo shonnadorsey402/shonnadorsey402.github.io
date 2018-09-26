2f9e9fb24b8741c6d2b94559174698a478f4da43
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:23:39 -0500

    Update script.js

--- sept26/shonna/workshop/js/script.js
+++ sept26/shonna/workshop/js/script.js
@@ -3,8 +3,7 @@ $(document).ready(function(){
     $('div').css({'border-radius':'10em'});
   });
   
-  $('#text').click(function(){
-    $('#custom-paragraph').css({'background-color':'yellow'});
+  $('#text').click(function() {
+    $('p').css({'background-color':'red'});
   });
-
-});
+ });
