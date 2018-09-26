41511afc26383087bbe95d865c5a0ebcf2d494e8
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:19:43 -0500

    Update script.js

--- sept26/bobbi/js/script.js
+++ sept26/bobbi/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+  $('#round-corners').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
+  
+  $('text').click(function(){
+    $('#custom-paragraph').css({'background-color':'magenta'});
+  });
 });
