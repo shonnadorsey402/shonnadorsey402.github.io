352298a1eb8947fe435c817c7776a5bf6861f118
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 09:45:24 -0500

    Create js/script.js

---
+++ sept26/shonna/workshop/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#round-corners').click(function(){
+    $('div').css({'border-radius', '10em'});
+  });
+  
+  $(#text).click(function(){
+    $('#custom-paragraph).css({'background-color','yellow'});
+  });
+
+});
