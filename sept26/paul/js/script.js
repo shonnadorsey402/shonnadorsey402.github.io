4cab19c54087a0731ed7d48b7afccc094339e535
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-26 14:22:10 -0500

    Update script.js

--- sept26/paul/js/script.js
+++ sept26/paul/js/script.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
 $('#round-corners'.click(function(){
   $('div').css({'border-radius':'10em'});
 });
+
+$('#text').click(function() {
+  $('p').css({background-color':'red'});
+});
