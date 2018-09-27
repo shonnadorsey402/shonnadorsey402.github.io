158483f5d6172ad8b09e79e0a02298a472be8375
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 14:35:48 -0500

    Update script.js

--- sept27/veronica/js/script.js
+++ sept27/veronica/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-$('button').click(function(){
+$('#hide-image').click(function(){
   $('img').hide();
 });
+  $('#test').click(function(){
+    $('p').css({'background-color':'blue'});
+  });
+  $('#round-corners').click(function(){
+    $('div').css({'border-radius':'100px'}):
+  });
+  
   $('div').click(function(){
     $(this).hide();
   });
