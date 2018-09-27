6736447bdd67a79fe530606b44d573192e9546d3
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 11:46:00 -0500

    Add files via upload

---
+++ sept27/shonna/workshop/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('#round-corners').click(function(){
+    $('div').css({'border-radius':'10em'});
+  });
+  
+  $('#text').click(function() {
+    $('p').css({'background-color':'red'});
+  });
+  
+  $('img').click(function() {
+    $(this).toggle('explode');
+  });
+ });
