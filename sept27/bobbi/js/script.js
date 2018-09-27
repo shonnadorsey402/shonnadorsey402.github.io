87dfb06b63982ea11614a3121b1a664f31c6511d
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 12:57:08 -0500

    Update script.js

--- sept27/bobbi/js/script.js
+++ sept27/bobbi/js/script.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-  
-  $('#hide-images').click(function(){
-    $('img').toggle('slow');
-  });
-  
-  $('#page-recolor').click(function(){
-    $('body').css({'background-color':'orange'});
-  });
+
 });
+
