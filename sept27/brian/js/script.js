cf463b9ebed87418cd8988d183905ecc7bbad93f
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 12:57:27 -0500

    Update script.js

--- sept27/brian/js/script.js
+++ sept27/brian/js/script.js
@@ -1,13 +1,3 @@
 $(document).ready(function(){
-$('#round-corners').click(function(){
-  $('div').css({'border-radius';'10em'});
-});
-$('#text').click(function() {
-  $('p').css({'background-color';'red'});
-});
 
-
-$('img').click(function() {
-  $(this).toggle('explode');
-});
 });
