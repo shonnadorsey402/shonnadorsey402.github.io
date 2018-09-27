232af826ce7cf3dad4964473415114853b0541c6
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 14:35:41 -0500

    Update script.js

--- sept27/bev/js/script.js
+++ sept27/bev/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
-$('button').click(function(){
+$('#hide-image').click(function(){
   $('img').hide();
 });
+  $('#text').click(function(){
+    $('p').css({'background-color':'orange'});
+  });
+  $('#round-corners').click(function(){
+    $('div').css({'border-radius':'100px'});
+  });
+  
   $('div').click(function(){
     $(this).hide();
   });
