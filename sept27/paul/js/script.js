3d42ad98755949aac91d2a46e8769dfb4ad5ee6b
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 15:55:10 -0500

    Update script.js

--- sept27/paul/js/script.js
+++ sept27/paul/js/script.js
@@ -2,11 +2,19 @@ $(document).ready(function(){
   $('#hide-image-button').click(function(){
     $('img').hide();
   });
+  
+  $('#body').click(function(){
+    $('body').css({'background-color': #644143});
+
+  
+  
+  
+  
   $('#text').click(function(){
     $('p').css({'background-color':'red'});
   });
   $('#round-corners').click(function(){
     $('div').css({'border-radius':'10em'});
   });
-});
+ }); 
   
