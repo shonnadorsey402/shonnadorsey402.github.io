4d3f0444c8d8b859dbb9d542c43ac56c6ceb022e
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-09-27 15:27:00 -0500

    Update script.js

--- sept27/jacque/js/script.js
+++ sept27/jacque/js/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 $('button').click(function(){
   $('img').hide();
     });
-});
   
   $('body').click(function(){
     $('body').css({'background-image':'url('https://www.backgroundsy.com/file/large/spiral-rainbow-background.jpg')});
