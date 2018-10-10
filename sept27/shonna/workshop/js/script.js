309c77d9890e7c232b67f4cb46b0768c153a9c7c
shonnadorsey402 <43411304+shonnadorsey402@users.noreply.github.com>
2018-10-10 14:12:51 -0500

    Update script.js

--- sept27/shonna/workshop/js/script.js
+++ sept27/shonna/workshop/js/script.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
-  $('#hide-image').click(function(){
-    $('img').hide();
-  });
-  $('#text').click(function(){
-    $('p').css({'background-color':'red'});
-  });
-  $('#round-corners').click(function(){
-    $('div').css({'border-radius':'100px'});
-  });
-  
-  
-  $('div').click(function(){
-    $(this).hide();
-  });
+
 });
